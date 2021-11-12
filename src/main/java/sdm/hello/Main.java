ab2bcf206763c16608893bad9ed8eedc26ba2230
Riccardo Zulla <riccardozulla@gmail.com>
2021-11-12 15:37:26 +0100

    Init commit

---
+++ src/main/java/sdm/hello/Main.java
@@ -0,0 +1,11 @@
+package sdm.hello;
+
+public class Main {
+  public static void main (String... args){
+    if(args.length>0){
+      System.out.println("Hello" + args[0] + "!");
+    }else{
+      System.out.println("Hello stranger!");
+    }
+  }
+}
